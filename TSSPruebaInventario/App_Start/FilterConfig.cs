15748ce04c174d30796039fe4d8d3e13d8d65603
Armando Martinez <Mr_Armando_Cacata@live.com>
2018-02-07 00:19:30 -0400

    Practica de inventario de la TSS para la evaluación del puesto de porgramador II.

---
+++ TSSPruebaInventario/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TSSPruebaInventario
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TSSPruebaInventario/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TSSPruebaInventario.MvcApplication" Language="C#" %>
